5fe88c57c43a2d8239a46ce3722818e3c8ef30b9
Johann <johann@dotgee.fr>
2012-10-01 18:21:06 +0200

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhozimmoCommunityEngine::Application.config.secret_token = '56b7f21ac27470d32b5ebc09bd0bad9eb02c0a17ac19e5c9fb51d7234b517a00b3fbf07ba13a3a3a043241bb0ff603e374835ec63b18ea7b864f1c0412cabb77'
